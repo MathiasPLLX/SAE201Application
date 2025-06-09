924af79f712657cbbdba81cfec61ba4aaa493454
Siyenn <cian.rimond@gmail.com>
2025-06-09 16:20:11 +0200

    Interface ICrud + Implémentation dans Vin + Création de VinsTableau (récupération des données dans la base)

---
+++ SAE201/ClassesVues/VinsTableau.cs
@@ -0,0 +1,43 @@
+﻿using SAE201.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAE201.ClassesVues
+{
+    public class VinsTableau
+    {
+        public ObservableCollection<VinAffichage> Vins { get; set; }
+
+        public VinsTableau()
+        {
+            Vins = new ObservableCollection<VinAffichage>(TousLesVins());
+        }
+
+        public List<VinAffichage> TousLesVins()
+        {
+            List<VinAffichage> liste = new List<VinAffichage>();
+
+            List<Vin> vinsBdd = new Vin().FindAll();
+
+            foreach (var vin in vinsBdd)
+            {
+                liste.Add(new VinAffichage
+                {
+                    NumVin = vin.NumVin,
+                    NomVin = vin.NomVin,
+                    NomAppelation = vin.Appelation?.NomAppelation, // sécurité si Appelation est null
+                    NomType = vin.TypeVin?.NomType,                 // sécurité si TypeVin est null
+                    PrixVin = vin.PrixVin ?? 0,                    // sécurité si PrixVin est null
+                    Millesime = vin.Millesime ?? 0                 // sécurité si Millesime est null
+                });
+            }
+
+            return liste;
+        }
+    }
+}
---
+++ SAE201/Model/ICrud.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAE201.Model
+{
+    public interface ICrud<T>
+    {
+        int Create();
+        void Read();
+        int Update();
+        int Delete();
+        List<T> FindAll();
+        List<T> FindBySelection(string criteres);
+    }
+}
+
