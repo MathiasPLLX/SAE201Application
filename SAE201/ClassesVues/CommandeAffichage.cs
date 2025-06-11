72f7840c53d593b2d61ec5bbba7a5b8921bd0899
Mathias Pailloux <mathias.pailloux@etu.univ-smb.fr>
2025-06-11 10:06:28 +0200

    Création CommandeAffichage

---
+++ SAE201/ClassesVues/CommandeAffichage.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace SAE201.ClassesVues
+{
+    internal class CommandeAffichage : INotifyPropertyChanged
+    {
+
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
